344a3a5d8f0ab90abee6f00924177bfca1fc5faf
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-03-24 20:37:27 -0300

    Iniciando com testes automautomatizados - Boas práticas com testes unitários

--- code-micro-videos/tests/Unit/CategoryTest.php
+++ code-micro-videos/tests/Unit/CategoryTest.php
@@ -3,21 +3,48 @@
 namespace Tests\Unit;
 
 use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use PHPUnit\Framework\TestCase;
 
 class CategoryTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testCategory()
+
+    public function testFillableAttribute()
+    {
+        $fillable = ['name', 'description', 'is_active'];
+        $category = new Category();
+        $this->assertEquals($fillable, $category->getFillable());
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $category = new Category();
+        $categoryDates = $category->getDates();
+        foreach ($dates as $date) {
+            $this->assertContains($date, $categoryDates);
+        }
+        $this->assertCount(count($dates), $categoryDates);
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [SoftDeletes::class, Uuid::class];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        $this->assertEquals($traits, $categoryTraits);
+    }
+
+
+    public function testKeyTypeAttribute()
     {
         $category = new Category();
-        $this->assertEquals(
-            ['name', 'description', 'is_active'],
-            $category->getFillable()
-        );
+        $this->assertEquals('string', $category->getKeyType());
+    }
+
+    public function testIncrementing()
+    {
+        $category = new Category();
+        $this->assertFalse($category->getIncrementing());
     }
 }
