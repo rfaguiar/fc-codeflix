b0b48137e232b45063c8e81c53faf25f86bddc4c
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-04-02 19:29:06 -0300

    Iniciando com testes automatizados no laravel - Testando criação de categoria

--- code-micro-videos/tests/Feature/ExampleTest.php
+++ code-micro-videos/tests/Feature/ExampleTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
@@ -21,5 +20,40 @@ class ExampleTest extends TestCase
             ['id', 'name', 'description', 'is_active', 'deleted_at', 'created_at', 'updated_at'],
             $categoryKey
         );
+    }
+
+    public function testCreate()
+    {
+        $category = Category::create(['name' => 'test1']);
+        $category->refresh();
+        $this->assertEquals('test1', $category->name);
+        $this->assertNull($category->description);
+        $this->assertTrue((bool)$category->is_active);
+
+        $category = Category::create([
+            'name' => 'test1',
+            'description' => null
+        ]);
+        $this->assertNull($category->description);
+
+        $category = Category::create([
+            'name' => 'test1',
+            'description' => 'test_description'
+        ]);
+        $this->assertEquals('test_description', $category->description);
+
+
+        $category = Category::create([
+            'name' => 'test1',
+            'is_active' => false
+        ]);
+        $this->assertFalse((bool)$category->is_active);
+
+        $category = Category::create([
+            'name' => 'test1',
+            'is_active' => true
+        ]);
+        $this->assertTrue((bool)$category->is_active);
+
     }
 }
