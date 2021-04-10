e516bfe049a93622cfe18c8260502dd25392db9d
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-04-10 16:58:43 -0300

    Iniciando recursos de vídeo - Criando Resource de Vídeo

--- code-micro-videos/routes/api.php
+++ code-micro-videos/routes/api.php
@@ -21,4 +21,5 @@ Route::group(['namespace' => 'Api'], function (){
     Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
     Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
     Route::resource('cast_members', 'CastMemberController', $exceptCreateAndEdit);
+    Route::resource('Videos', 'VideoController', $exceptCreateAndEdit);
 });
