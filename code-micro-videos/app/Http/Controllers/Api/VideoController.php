99570138e61f0bf6f11034d7868ead5ee104b0eb
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-04-13 13:15:25 -0300

    Upload de arquivos - Parte1 - Sobre modos transações alinhados

--- code-micro-videos/app/Http/Controllers/Api/VideoController.php
+++ code-micro-videos/app/Http/Controllers/Api/VideoController.php
@@ -30,15 +30,9 @@ class VideoController extends BasicCrudController
 
     public function store(Request $request)
     {
-        $validatedData = $this->validate($request, $this->rulesStore());
         $this->addRuleIfGenreHasCategories($request);
-        $self = $this;
-        /** @var Video $obj */
-        $obj = \DB::transaction(function () use ($request, $validatedData, $self) {
-            $obj = $this->model()::create($validatedData);
-            $self->handleRelations($obj, $request);
-            return $obj;
-        });
+        $validatedData = $this->validate($request, $this->rulesStore());
+        $obj = $this->model()::create($validatedData);
         $obj->refresh();
         return $obj;
     }
@@ -48,12 +42,7 @@ class VideoController extends BasicCrudController
         $obj = $this->findOrFail($id);
         $this->addRuleIfGenreHasCategories($request);
         $validatedData = $this->validate($request, $this->rulesUpdate());
-        $self = $this;
-        $obj = \DB::transaction(function () use ($request, $obj, $validatedData, $self) {
-            $obj->update($validatedData);
-            $self->handleRelations($obj, $request);
-            return $obj;
-        });
+        $obj->update($validatedData);
         return $obj;
     }
 
@@ -62,12 +51,6 @@ class VideoController extends BasicCrudController
         $categoriesId = $request->get('categories_id');
         $categoriesId = is_array($categoriesId) ? $categoriesId : [];
         $this->rules['genres_id'][] = new GenresHasCategoriesRule($categoriesId);
-    }
-
-    protected function handleRelations($video, Request $request)
-    {
-        $video->categories()->sync($request->get('categories_id'));
-        $video->genres()->sync($request->get('genres_id'));
     }
 
     protected function model()
---
+++ code-micro-videos/tests/Feature/Models/VideoTest.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Tests\Feature\Models;
+
+
+use App\Models\Video;
+use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class VideoTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testRollbackcreate()
+    {
+        $hasError = false;
+        try {
+            Video::create([
+                'title'=>'title',
+                'description'=>'description',
+                'year_launched'=>2010,
+                'rating'=>Video::RATING_LIST[0],
+                'duration'=>90,
+                'categories_id' => [0,1,2]
+            ]);
+        } catch (QueryException $exception) {
+            $this->assertCount(0, Video::all());
+            $hasError = true;
+        }
+        $this->assertTrue($hasError);
+    }
+
+    public function testRollbackUpdate()
+    {
+        $video = factory(Video::class)->create();
+        $oldTitle = $video->title;
+        $hasError = false;
+        try {
+            $video->update([
+                'title'=>'title',
+                'description'=>'description',
+                'year_launched'=>2010,
+                'rating'=>Video::RATING_LIST[0],
+                'duration'=>90,
+                'categories_id' => [0,1,2]
+            ]);
+        } catch (QueryException $exception) {
+            $this->assertDatabaseHas('videos', [
+                'title' => $oldTitle
+            ]);
+            $hasError = true;
+        }
+        $this->assertTrue($hasError);
+    }
+
+}
