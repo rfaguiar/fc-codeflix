47c357720bbc4e62c0d95a9ce64d5ac7804033fa
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-04-20 09:13:04 -0300

    Upload de arquivos - Parte2 - Testando método para excluir arquivos antigos do vídeo

--- code-micro-videos/tests/Stubs/Models/UploadFilesStub.php
+++ code-micro-videos/tests/Stubs/Models/UploadFilesStub.php
@@ -12,17 +12,17 @@ class UploadFilesStub extends Model
     use UploadFiles;
 
     public static $fileFields = ['file1', 'file2'];
-/*
+
     protected $table = 'upload_file_stubs';
     protected $fillable = ['name', 'file1', 'file2'];
 
-    public static function MakeTable()
+    public static function makeTable()
     {
         \Schema::create('upload_file_stubs', function ($table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('file1');
-            $table->string('file2');
+            $table->string('file1')->nullable();
+            $table->string('file2')->nullable();
             $table->timestamps();
         });
     }
@@ -30,7 +30,7 @@ class UploadFilesStub extends Model
     public static function dropTable()
     {
         \Schema::dropIfExists('upload_file_stubs');
-    }*/
+    }
 
     protected function uploadDir(): string
     {
