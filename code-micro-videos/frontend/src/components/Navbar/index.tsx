4c29a0ad456546eb15492e9a0e702bfa0baa6a1d
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-04-23 20:23:30 -0300

    Definindo layout do Front-end - Iniciando com temas e make Styles

--- code-micro-videos/frontend/src/components/Navbar/index.tsx
+++ code-micro-videos/frontend/src/components/Navbar/index.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import {AppBar, Button, makeStyles, Toolbar, Typography} from "@material-ui/core";
+import {AppBar, Button, makeStyles, Theme, Toolbar, Typography} from "@material-ui/core";
 import logo from '../../static/img/logo.png';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
         backgroundColor: '#000000'
     },
     title: {
         flexGrow: 1,
         textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]: {
+            width: 170
+        }
     }
-});
+}));
 
 export const Navbar: React.FC = () => {
     const classes = useStyles();
@@ -18,7 +24,7 @@ export const Navbar: React.FC = () => {
         <AppBar>
             <Toolbar className={classes.toolbar}>
                 <Typography className={classes.title}>
-                    <img src={logo} alt="CodeFlix"/>
+                    <img src={logo} alt="CodeFlix" className={classes.logo}/>
                 </Typography>
                 <Button color="inherit">Login</Button>
             </Toolbar>
