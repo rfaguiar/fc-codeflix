b27a3921ad08300c64390b168dd4fb5bdabc60a6
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-04-25 08:57:28 -0300

    Criando navegação Single Page application - Definindo componente para renderizar as rotas

--- code-micro-videos/frontend/src/App.tsx
+++ code-micro-videos/frontend/src/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar";
-import {Page} from "./components/Page";
 import {Box} from "@material-ui/core";
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
-      <>
+      <BrowserRouter>
         <Navbar/>
         <Box paddingTop={'70px'}>
-          <Page title={'Categorias'}>
-            Conteudo
-          </Page>
+          <AppRouter/>
         </Box>
-      </>
+      </BrowserRouter>
   );
 }
 
