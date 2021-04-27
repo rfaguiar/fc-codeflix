da588832b96e0ae132c43f3047a21193f751cac2
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-04-27 20:19:49 -0300

    Criando navegação Single Page application - Montando Breadcrumb

--- code-micro-videos/frontend/src/App.tsx
+++ code-micro-videos/frontend/src/App.tsx
@@ -4,13 +4,15 @@ import {Navbar} from "./components/Navbar";
 import {Box} from "@material-ui/core";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import Breadcrumbs from "./components/Breadcrumbs";
 
 function App() {
   return (
       <BrowserRouter>
         <Navbar/>
         <Box paddingTop={'70px'}>
-          <AppRouter/>
+            <Breadcrumbs/>
+            <AppRouter/>
         </Box>
       </BrowserRouter>
   );
--- code-micro-videos/frontend/src/routes/index.tsx
+++ code-micro-videos/frontend/src/routes/index.tsx
@@ -21,6 +21,20 @@ const routes: MyRouteProps[] = [
         path: '/categories',
         component: CategoryList,
         exact: true
+    },
+    {
+        name: 'categories.create',
+        label: 'Criar Categoria',
+        path: '/categories/create',
+        component: CategoryList,
+        exact: true
+    },
+    {
+        name: 'categories.edit',
+        label: 'Editar Categoria',
+        path: '/categories/:id/edit',
+        component: CategoryList,
+        exact: true
     }
 ];
 
