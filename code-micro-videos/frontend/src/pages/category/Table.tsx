0be54ed903cea86041be00a112c484fa845472c0
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-05-02 13:43:17 -0300

    Construindo listagem de categorias - Formatando Data Com Date-Fns

--- code-micro-videos/frontend/src/pages/category/Table.tsx
+++ code-micro-videos/frontend/src/pages/category/Table.tsx
@@ -3,6 +3,8 @@ import MUIDataTable, {MUIDataTableColumn} from "mui-datatables";
 import {useEffect, useState} from "react";
 import {httpVideo} from "../../util/http";
 import {Chip} from "@material-ui/core";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
 
 const columnsDefinition: MUIDataTableColumn[] = [
     {
@@ -20,7 +22,12 @@ const columnsDefinition: MUIDataTableColumn[] = [
     },
     {
         name: "created_at",
-        label: "Criado em"
+        label: "Criado em",
+        options: {
+            customBodyRender(value, tableMeta, updateValue) {
+                return <span>{format(parseISO(value), 'dd/MM/yyyy')}</span>;
+            }
+        }
     }
 ];
 
