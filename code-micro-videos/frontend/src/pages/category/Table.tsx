dc4ee4540e25b9c5918031c767b981e41094fa36
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-05-02 11:29:42 -0300

    Construindo listagem de categorias - Configurando Axios

--- code-micro-videos/frontend/src/pages/category/Table.tsx
+++ code-micro-videos/frontend/src/pages/category/Table.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import MUIDataTable, {MUIDataTableColumn} from "mui-datatables";
+import {useEffect, useState} from "react";
+import {httpVideo} from "../../util/http";
 
 const columnsDefinition: MUIDataTableColumn[] = [
     {
@@ -16,19 +18,16 @@ const columnsDefinition: MUIDataTableColumn[] = [
     }
 ];
 
-const data = [
-    {name: "teste1", is_active: true, created_at: "2019-12-12"},
-    {name: "teste2", is_active: false, created_at: "2019-12-13"},
-    {name: "teste3", is_active: false, created_at: "2019-12-14"},
-    {name: "teste4", is_active: true, created_at: "2019-12-15"},
-    {name: "teste5", is_active: false, created_at: "2019-12-16"},
-    {name: "teste6", is_active: true, created_at: "2019-12-17"},
-];
-
 type Props = {
 
 };
 const Table = (props: Props) => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        httpVideo.get('categories')
+            .then(response => setData(response.data.data));
+    }, []);
+
     return (
         <div>
             <MUIDataTable
